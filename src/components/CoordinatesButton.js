190feb447cd46caf392eb1361d648b1580977e4d
Laika Deunert <laikadeunert@MacBook-Air.local>
2022-02-20 21:45:19 -0800

    commit tree 7bf49e5aaf304507ddd25b2faff17dd4b8b6faea

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,27 @@
 // Code CoordinatesButton Component Here
+
+import React, { Component } from 'react'
+
+export default class CoordinatesButton extends Component {
+
+    constructor(prop) {
+        super(prop);
+        this.onReceiveCoordinates = this.onReceiveCoordinates.bind(this);
+    }
+
+    onReceiveCoordinates(event) {
+        const x = event.clientX;
+        const y = event.clientY;
+        const mouseCoordinates = [x,y];
+        this.props.onReceiveCoordinates(mouseCoordinates);
+    }
+    
+    render() {
+        return (
+        <div>
+            <button onClick={this.onReceiveCoordinates}>Submit</button>
+        </div>
+        )
+    }
+}
+
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,23 @@
 // Code DelayedButton Component Here
+
+import React, { Component } from 'react'
+
+export default class DelayedButton extends Component {
+
+  constructor(props) {
+    super(props);
+    this.onDelayedClick = this.sayHello.bind(this, this.props.delay);
+  }
+
+  onDelayedClick(event, delay) {
+
+  }
+
+  render() {
+    return (
+    <div>
+      <button onClick={this.onDelayedClick}>Submit</button>
+    </div>
+    )
+   }
+}
